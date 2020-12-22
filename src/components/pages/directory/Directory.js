da9bece56c9efd37a58772680f4d3fc85946bd19
John Carl James Nebab <ivan@orangeandcrane.com>
2020-12-22 14:48:30 +0800

    feat: add new Directory UI

--- src/components/pages/directory/Directory.js
+++ src/components/pages/directory/Directory.js
@@ -1,5 +1,28 @@
-function Directory(props) {
-  return <p>Directory page</p>
+/* eslint-disable react/jsx-key */
+import React, { useState } from 'react'
+import { Card, Tabs } from '@app/components/globals'
+import ActiveDummyTable from './DummyTable'
+
+const tabs = [
+  { label: 'Directory', value: 1 },
+  { label: 'Manage Directory', value: 2 }
+]
+
+function Directory() {
+  const [activeTab, setActiveTab] = useState(1)
+
+  return (
+    <section className={'content-wrap py-5 px-8'}>
+      <h1 className="content-title mb-5">Directory</h1>
+      <div className="toolbar">
+        <Tabs tabs={tabs} activeTab={activeTab} handleTab={setActiveTab} />
+      </div>
+      <Card
+        title="Companies"
+        content={<ActiveDummyTable active={activeTab} />}
+      />
+    </section>
+  )
 }
 
 export default Directory
