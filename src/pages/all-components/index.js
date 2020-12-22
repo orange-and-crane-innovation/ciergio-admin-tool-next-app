ad1d34b8f1db4c36553a9da813e738e7a36ed1a3
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2020-12-22 09:22:01 +0800

    chore: addded new components

--- src/pages/all-components/index.js
+++ src/pages/all-components/index.js
@@ -1,14 +1,25 @@
 import FormsComponents from './Forms'
 import CardsComponents from './Cards'
+import PaginationComponents from './Pagination'
+import ToastComponents from './Toast'
 
 function ComponentsPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-5">Form Components</h1>
       <FormsComponents />
+
       <hr className="py-6" />
       <h1 className="text-2xl font-semibold mb-5">Card Component</h1>
       <CardsComponents />
+
+      <hr className="py-6" />
+      <h1 className="text-2xl font-semibold mb-5">Pagination Component</h1>
+      <PaginationComponents />
+
+      <hr className="py-6" />
+      <h1 className="text-2xl font-semibold mb-5">Toast Component</h1>
+      <ToastComponents />
     </div>
   )
 }
