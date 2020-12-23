e188d1b55aed9071aa8d11c32a9968133bd659be
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2020-12-23 10:51:24 +0800

    chore: updated card component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 
 import styles from './card.module.css'
 
-const Card = ({ header, content, footer, noBorder }) => {
-  const contentClass = noBorder
-    ? `${styles.cardContentControl} ${styles.noBorder}`
+const Card = ({ header, content, footer, noPadding }) => {
+  const contentClass = noPadding
+    ? `${styles.cardContentControl} ${styles.noPadding}`
     : styles.cardContentControl
 
   return (
     <div className={styles.cardContainer}>
       {header && <div className={styles.cardHeaderControl}>{header}</div>}
       <div className={contentClass}>{content}</div>
-      <div className={styles.cardFooterControl}>{footer}</div>
+      {footer && <div className={styles.cardFooterControl}>{footer}</div>}
     </div>
   )
 }
@@ -21,7 +21,7 @@ Card.propTypes = {
   header: PropTypes.any,
   content: PropTypes.any,
   footer: PropTypes.any,
-  noBorder: PropTypes.bool
+  noPadding: PropTypes.bool
 }
 
 export default Card
