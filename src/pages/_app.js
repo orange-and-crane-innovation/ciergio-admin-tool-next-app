bad5a622b5f29edfb4ca1f9fa655a56c5271c822
John Carl James Nebab <ivan@orangeandcrane.com>
2020-12-24 13:50:52 +0800

    chore: updated Tabs and Cards components css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 // Styles
 import '@app/styles/globals.css'
-// import '@app/styles/temp-main-content.css'
+import '@app/styles/temp-main-content.css'
 import '@app/styles/ciergio-icon.css'
 import '@app/styles/animate.css'
 import '@app/styles/components/buttons.css'
