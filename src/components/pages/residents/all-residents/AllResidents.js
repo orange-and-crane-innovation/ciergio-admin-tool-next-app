befd7d458d74b336bb960b248c13146bae90a809
John Carl James Nebab <ivan@orangeandcrane.com>
2021-01-11 12:19:55 +0800

    refactor: modify AllResidents module to use new Table component

--- src/components/pages/residents/all-residents/AllResidents.js
+++ src/components/pages/residents/all-residents/AllResidents.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
+import P from 'prop-types'
 
 import FormSelect from '@app/components/forms/form-select'
 import FormInput from '@app/components/forms/form-input'
 import Button from '@app/components/button'
-import Table from '@app/components/table'
-import Pagination from '@app/components/pagination'
+import Table from '@app/components/globals/Table'
 import { Card } from '@app/components/globals'
 
 import { FaTimes, FaSearch, FaPlusCircle } from 'react-icons/fa'
 import { HiOutlinePrinter } from 'react-icons/hi'
 import { FiDownload } from 'react-icons/fi'
+import { GoKebabHorizontal } from 'react-icons/go'
+
 import AddResidentModal from '../AddResidentModal'
 
 const floors = [
@@ -42,24 +44,108 @@ const accountTypes = [
   }
 ]
 
-const tableRows = [
-  {
-    name: 'Unit #',
-    width: ''
-  },
-  {
-    name: 'Resident Name',
-    width: ''
-  },
-  {
-    name: 'Account Type',
-    width: ''
-  }
-]
-
 function AllResidents() {
   const [searchText, setSearchText] = useState('')
   const [showModal, setShowModal] = useState('')
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Unit #',
+        accessor: 'unit_number'
+      },
+      {
+        Header: 'Resident Name',
+        accessor: row => row,
+        Cell: ResidentCell
+      },
+      {
+        Header: 'Account Type',
+        accessor: row => row,
+        Cell: ResidentType
+      },
+      {
+        id: 'invite',
+        accessor: row => row,
+        Cell: ResidentInviteButton
+      },
+      {
+        id: 'action',
+        accessor: row => row,
+        Cell: ResidentAction
+      }
+    ],
+    []
+  )
+
+  const residentsData = React.useMemo(
+    () => [
+      {
+        unit_number: 101,
+        resident_name: 'Brandie Lane',
+        contact_number: '09778562090',
+        account_type: 'Unit Owner',
+        active: true
+      },
+      {
+        unit_number: 101,
+        resident_name: 'Max Murphy',
+        contact_number: '09778562090',
+        account_type: 'Relative',
+        active: false
+      },
+      {
+        unit_number: 102,
+        resident_name: 'Ralph Bell',
+        contact_number: '09778562090',
+        account_type: 'Other',
+        active: false
+      },
+      {
+        unit_number: 103,
+        resident_name: 'Victoria Miles',
+        contact_number: '09778562090',
+        account_type: 'Unit Owner',
+        active: true
+      },
+      {
+        unit_number: 103,
+        resident_name: 'Wendy Jones',
+        contact_number: '09778562090',
+        account_type: 'Staff',
+        active: true
+      },
+      {
+        unit_number: 104,
+        resident_name: 'Judith Cooper',
+        contact_number: '09778562090',
+        account_type: 'Staff',
+        active: true
+      },
+      {
+        unit_number: 105,
+        resident_name: 'Wendy Jones',
+        contact_number: '09778562090',
+        account_type: 'Unit Owner',
+        active: true
+      },
+      {
+        unit_number: 105,
+        resident_name: 'Albert Howard',
+        contact_number: '09778562090',
+        account_type: 'Housemate',
+        active: true
+      },
+      {
+        unit_number: 105,
+        resident_name: 'Priscilla Richards',
+        contact_number: '09778562090',
+        account_type: 'Housemate',
+        active: false
+      }
+    ],
+    []
+  )
 
   const handleShowModal = () => setShowModal(old => !old)
 
@@ -115,16 +201,63 @@ function AllResidents() {
           />
         </div>
       </div>
-      <Card content={<Table rowNames={tableRows} items={[]} />} />
-      <Pagination
-        items={{ count: 10, limit: 10, offset: 0, data: [] }}
-        activePage={1}
-        onPageClick={e => alert('Page ' + e)}
-        onLimitChange={e => alert('Show ' + e.target.value)}
-      />
+      <Card content={<Table columns={columns} data={residentsData} />} />
       <AddResidentModal showModal={showModal} onShowModal={handleShowModal} />
     </section>
   )
+}
+
+function ResidentCell({ value }) {
+  return (
+    <div>
+      <div>{value.resident_name}</div>
+      <div>{value.contact_number}</div>
+    </div>
+  )
+}
+
+function ResidentAction({ value }) {
+  return (
+    <div className="w-full flex justify-end pr-8">
+      <GoKebabHorizontal onClick={() => console.log({ value })} />
+    </div>
+  )
+}
+
+function ResidentInviteButton({ value }) {
+  if (value.active) return null
+
+  return (
+    <div>
+      <Button label="Invite" onClick={() => console.log('invited')} />
+    </div>
+  )
+}
+
+function ResidentType({ value }) {
+  return (
+    <div>
+      <span>{`${value.account_type} ${
+        !value.active ? '(Unregistered)' : ''
+      }`}</span>
+    </div>
+  )
+}
+
+ResidentCell.propTypes = {
+  value: P.object
+}
+
+ResidentAction.propTypes = {
+  value: P.object
+}
+
+ResidentInviteButton.propTypes = {
+  value: P.object
+}
+
+ResidentType.propTypes = {
+  value: P.object
 }
 
 export default AllResidents
