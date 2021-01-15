787e96721922b8f9b30c8075037edadb76e8dcae
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-01-15 08:59:17 +0800

    chore: added modal animation

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,5 +1,10 @@
+import { DialogOverlay, DialogContent } from '@reach/dialog'
+import { useTransition, animated } from 'react-spring'
 import P from 'prop-types'
+
 import Button from '@app/components/button'
+
+import '@reach/dialog/styles.css'
 
 function Component({
   title,
@@ -12,37 +17,69 @@ function Component({
   footer,
   children
 }) {
+  const AnimatedDialogOverlay = animated(DialogOverlay)
+  const AnimatedDialogContent = animated(DialogContent)
+  const transitions = useTransition(visible, null, {
+    from: { opacity: 0, y: -10 },
+    enter: { opacity: 1, y: 0 },
+    leave: { opacity: 0, y: 10 }
+  })
+
   return (
-    <div className={['modal-overlay', visible ? 'flex' : 'hidden'].join(' ')}>
-      <div className="modal">
-        <div className="modal-header">
-          <div className="modal-title">
-            <span>{title}</span>
-          </div>
-          <div className="modal-close-icon">
-            <span
-              className="ciergio-close absolute p-4 hover:cursor-pointer relative top-0"
-              onClick={onClose}
-              onKeyDown={onClose}
-              role="button"
-              tabIndex={0}
-            />
-          </div>
-        </div>
-        <div className="modal-content">{children}</div>
-        {footer !== null ? (
-          <div className="modal-footer">
-            <Button
-              default
-              label={cancelText}
-              onClick={onCancel}
-              className="mr-4 w-full"
-            />
-            <Button primary label={okText} onClick={onOk} className=" w-full" />
-          </div>
-        ) : null}
-      </div>
-    </div>
+    <>
+      {transitions.map(
+        ({ item, key, props: styles }) =>
+          item && (
+            <AnimatedDialogOverlay
+              key={key}
+              style={{ opacity: styles.opacity, zIndex: 100 }}
+            >
+              <AnimatedDialogContent
+                aria-label="modal"
+                style={{
+                  transform: styles.y.interpolate(
+                    value => `translate3d(0px, ${value}px, 0px)`
+                  )
+                }}
+              >
+                <div className="modal">
+                  <div className="modal-header">
+                    <div className="modal-title">
+                      <span>{title}</span>
+                    </div>
+                    <div className="modal-close-icon">
+                      <span
+                        className="ciergio-close absolute p-4 hover:cursor-pointer relative top-0"
+                        onClick={onClose}
+                        onKeyDown={onClose}
+                        role="button"
+                        tabIndex={0}
+                      />
+                    </div>
+                  </div>
+                  <div className="modal-content">{children}</div>
+                  {footer !== null ? (
+                    <div className="modal-footer">
+                      <Button
+                        default
+                        label={cancelText}
+                        onClick={onCancel}
+                        className="mr-4 w-full"
+                      />
+                      <Button
+                        primary
+                        label={okText}
+                        onClick={onOk}
+                        className=" w-full"
+                      />
+                    </div>
+                  ) : null}
+                </div>
+              </AnimatedDialogContent>
+            </AnimatedDialogOverlay>
+          )
+      )}
+    </>
   )
 }
 
@@ -55,7 +92,7 @@ Component.propTypes = {
   title: P.string,
   visible: P.bool.isRequired,
   onClose: P.func.isRequired,
-  children: P.string || P.element,
+  children: P.any,
   okText: P.string,
   cancelText: P.string,
   onOk: P.func,
