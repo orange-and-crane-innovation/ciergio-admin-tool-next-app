3f48f2e6f727d4c09427f5d5f9de214e9e8761f1
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-01-15 08:56:15 +0800

    chore: update common components

--- src/components/forms/form-select/FormSelect.js
+++ src/components/forms/form-select/FormSelect.js
@@ -1,13 +1,38 @@
 /* eslint-disable jsx-a11y/no-onchange */
-import styles from './FormSelect.module.css'
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import { FaTimes } from 'react-icons/fa'
 
-const InputSelect = ({ options, onChange, disabled, className }) => {
+import styles from './FormSelect.module.css'
+
+const InputSelect = ({
+  id,
+  name,
+  value,
+  options,
+  onChange,
+  onClear,
+  disabled,
+  noCloseIcon,
+  className
+}) => {
+  const inputClasses = useMemo(
+    () =>
+      clsx(styles.FormSelect, {
+        [styles.disabled]: disabled,
+        [className]: !!className
+      }),
+    [disabled, className]
+  )
+
   return (
-    <React.Fragment>
+    <div className={styles.FormSelectContainer}>
       <select
-        className={`${styles.FormSelect} ${styles.className} ${className}`}
+        className={inputClasses}
+        id={id}
+        name={name}
+        value={value}
         onChange={onChange}
         disabled={disabled}
       >
@@ -19,14 +44,24 @@ const InputSelect = ({ options, onChange, disabled, className }) => {
           )
         })}
       </select>
-    </React.Fragment>
+      {!noCloseIcon && (
+        <span className={styles.FormSelectIcon}>
+          {value && <FaTimes className="cursor-pointer" onClick={onClear} />}
+        </span>
+      )}
+    </div>
   )
 }
 
 InputSelect.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.any,
   options: PropTypes.array.isRequired,
   onChange: PropTypes.func,
+  onClear: PropTypes.func,
   disabled: PropTypes.bool,
+  noCloseIcon: PropTypes.bool,
   className: PropTypes.string
 }
 
