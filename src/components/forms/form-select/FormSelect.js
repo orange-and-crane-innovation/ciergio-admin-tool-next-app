03452b2f64a355e9aed6d46bbaefdab263bb69b7
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-01-27 12:41:34 +0800

    refactor: form select component

--- src/components/forms/form-select/FormSelect.js
+++ src/components/forms/form-select/FormSelect.js
@@ -1,8 +1,7 @@
 /* eslint-disable jsx-a11y/no-onchange */
-import React, { useMemo } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import clsx from 'clsx'
-import { FaTimes } from 'react-icons/fa'
+import Select, { components } from 'react-select'
 
 import styles from './FormSelect.module.css'
 
@@ -10,47 +9,82 @@ const InputSelect = ({
   id,
   name,
   value,
+  placeholder,
+  valueholder,
   options,
+  error,
   onChange,
   onClear,
   disabled,
   noCloseIcon,
   className,
-  description
+  description,
+  isMulti,
+  isClearable
 }) => {
-  const inputClasses = useMemo(
-    () =>
-      clsx(styles.FormSelect, {
-        [styles.disabled]: disabled,
-        [className]: !!className
-      }),
-    [disabled, className]
-  )
+  // eslint-disable-next-line react/prop-types
+  const ValueContainer = ({ children, ...props }) => {
+    // eslint-disable-next-line react/prop-types
+    const { getValue, hasValue } = props
+    const count = getValue().length
+    if (!hasValue) {
+      return (
+        <components.ValueContainer {...props}>
+          {children}
+        </components.ValueContainer>
+      )
+    }
+    return (
+      <components.ValueContainer {...props}>
+        {`${valueholder} (${count})`}
+      </components.ValueContainer>
+    )
+  }
 
   return (
     <div className={styles.FormSelectContainer}>
       {description || null}
-      <select
-        className={inputClasses}
+      <Select
+        styles={{
+          control: styles => ({
+            ...styles,
+            borderColor: error ? 'red' : styles.borderColor
+          })
+        }}
+        classNamePrefix="FormSelect"
         id={id}
         name={name}
+        placeholder={placeholder}
+        options={options}
+        noOptionsMessage={() => 'No item found.'}
         value={value}
-        onChange={onChange}
         disabled={disabled}
-      >
-        {options.map((item, index) => {
-          return (
-            <option key={index} value={item.value}>
-              {item.label}
-            </option>
-          )
-        })}
-      </select>
-      {!noCloseIcon && (
-        <span className={styles.FormSelectIcon}>
-          {value && <FaTimes className="cursor-pointer" onClick={onClear} />}
-        </span>
-      )}
+        components={
+          isMulti
+            ? {
+                ValueContainer,
+                IndicatorSeparator: () => null
+              }
+            : {
+                IndicatorSeparator: () => null
+              }
+        }
+        menuPlacement="auto"
+        closeMenuOnSelect={!isMulti}
+        hideSelectedOptions={false}
+        isMulti={isMulti}
+        isDisabled={disabled}
+        isClearable={isClearable}
+        isSearchable
+        onChange={(selectedOption, triggeredAction) => {
+          if (triggeredAction.action === 'clear') {
+            onClear()
+          } else {
+            onChange(selectedOption)
+          }
+        }}
+      />
+      <div className={styles.FormSelectError}>{error}</div>
     </div>
   )
 }
@@ -59,13 +93,18 @@ InputSelect.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   value: PropTypes.any,
+  placeholder: PropTypes.string,
+  valueholder: PropTypes.string,
   options: PropTypes.array.isRequired,
+  error: PropTypes.string,
   onChange: PropTypes.func,
   onClear: PropTypes.func,
   disabled: PropTypes.bool,
   noCloseIcon: PropTypes.bool,
   className: PropTypes.string,
-  description: PropTypes.node || PropTypes.string
+  description: PropTypes.node || PropTypes.string,
+  isMulti: PropTypes.bool,
+  isClearable: PropTypes.bool
 }
 
 export default InputSelect
