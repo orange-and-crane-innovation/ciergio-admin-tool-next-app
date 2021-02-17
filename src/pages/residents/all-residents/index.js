74c068228d342e9e714ed32c7b2e38df03d69903
John Carl James Nebab <ivan@orangeandcrane.com>
2021-02-17 15:50:27 +0800

    feat: use Can component in rendering Residents list

--- src/pages/residents/all-residents/index.js
+++ src/pages/residents/all-residents/index.js
@@ -1,7 +1,18 @@
 import AllResidents from '@app/components/pages/residents/all-residents'
+import Can from '@app/permissions/can'
 
 function AllResidentsPage() {
-  return <AllResidents />
+  return (
+    <Can
+      perform="residents:view"
+      yes={<AllResidents />}
+      no={
+        <div className="w-full flex items-center justify-center h-full bg-neutral-100">
+          <h1>Ooppps! Error 401: Unauthorized!</h1>
+        </div>
+      }
+    />
+  )
 }
 
 export default AllResidentsPage
