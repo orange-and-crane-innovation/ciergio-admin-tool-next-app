12e8e9bd0933716a6a48c68159a6a5a367766b8c
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-02-26 10:01:21 +0800

    chore: updated Login and Profile page

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -2,7 +2,9 @@ import { gql, useMutation } from '@apollo/client'
 import { useCallback, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-import Login from '@app/components/pages/login'
+import showToast from '@app/utils/toast'
+
+import Login from '@app/components/pages/auth/login'
 
 const LOGIN_MUTATION = gql`
   mutation Login($email: String!, $password: String!) {
@@ -16,19 +18,27 @@ const LOGIN_MUTATION = gql`
 
 function LoginPage() {
   const router = useRouter()
-  const [login, { loading, data, client }] = useMutation(LOGIN_MUTATION, {
-    onError: _e => {},
-    onCompleted: ({ login }) => {
-      localStorage.setItem('keep', login.slave)
-      client.resetStore()
+  const [login, { loading, data, client, called, error }] = useMutation(
+    LOGIN_MUTATION,
+    {
+      onError: _e => {},
+      onCompleted: ({ login }) => {
+        localStorage.setItem('keep', login.slave)
+        client.resetStore()
+      }
     }
-  })
+  )
 
   useEffect(() => {
-    if (!loading && data) {
-      router.replace('/dashboard')
+    if (!loading) {
+      if (error) {
+        errorHandler(error)
+      }
+      if (called && data) {
+        router.replace('/dashboard')
+      }
     }
-  }, [data, loading, router])
+  }, [loading, called, data, error, router])
 
   const onLoginSubmit = useCallback(
     values => {
@@ -42,6 +52,26 @@ function LoginPage() {
     },
     [login]
   )
+
+  const errorHandler = data => {
+    const errors = JSON.parse(JSON.stringify(data))
+
+    if (errors) {
+      const { graphQLErrors, networkError, message } = errors
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          showToast('danger', message)
+        )
+
+      if (networkError) {
+        showToast('danger', errors?.networkError?.result?.errors[0]?.message)
+      }
+
+      if (message) {
+        showToast('danger', message)
+      }
+    }
+  }
 
   return <Login onLoginSubmit={onLoginSubmit} isSubmitting={loading} />
 }
