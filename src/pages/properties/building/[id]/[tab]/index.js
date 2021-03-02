ebef40e41be57ce16d08bad71122d82b895e1fed
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-03-02 14:42:57 +0800

    chore: fix issue on routing

--- src/pages/properties/building/[id]/[tab]/index.js
+++ src/pages/properties/building/[id]/[tab]/index.js
@@ -1,22 +1,7 @@
-import { useRouter } from 'next/router'
-
 import BuildingDataComponent from '@app/components/pages/properties/building/buildingData'
 
 function BuildingSinglePropertiesPage() {
-  const router = useRouter()
-  const user = JSON.parse(localStorage.getItem('profile'))
-  const accountType = user?.accounts?.data[0]?.accountType
-
-  if (accountType === 'administrator') {
-    return <BuildingDataComponent />
-  } else if (accountType === 'company_admin') {
-    const id = user?.accounts?.data[0]?.company?._id
-    router.push(`/properties/company/${id}/overview`)
-  } else if (accountType === 'complex_admin') {
-    const id = user?.accounts?.data[0]?.complex?._id
-    router.push(`/properties/complex/${id}/overview`)
-  }
-  return null
+  return <BuildingDataComponent />
 }
 
 export default BuildingSinglePropertiesPage
