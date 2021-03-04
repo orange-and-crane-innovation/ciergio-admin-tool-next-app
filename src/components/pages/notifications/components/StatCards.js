4ac5d0d90a647557867992ddff825f761b327d0a
John Carl James Nebab <ivan@orangeandcrane.com>
2021-03-04 13:17:16 +0800

    style: move classes used inline to css file

--- src/components/pages/notifications/components/StatCards.js
+++ src/components/pages/notifications/components/StatCards.js
@@ -5,21 +5,21 @@ import styles from '../notifications.module.css'
 export default function StatCards() {
   return (
     <div className={styles.statCards}>
-      <div className="col-span-2 col-start-1 col-end-3 flex justify-start">
-        <div className="w-64 h-32 box-border p-0 bg-white mr-4 shadow-sm rounded border border-neutral-300 flex">
+      <div className={styles.statCardsContainer}>
+        <div className={styles.statCard}>
           <div className={styles.statCardIconContainer}>
             <GiAlarmClock className="h-8 w-8 text-white" />
           </div>
-          <div className="flex justify-center flex-col pl-4">
+          <div className={styles.statCardContent}>
             <h2 className="font-medium">Notifications Today</h2>
             <p className="text-2xl font-bold">43</p>
           </div>
         </div>
-        <div className="w-64 h-32 box-border p-0 bg-white shadow-sm rounded border border-neutral-300 flex">
+        <div className={styles.statCard}>
           <div className={styles.statCardIconContainer}>
             <GoWatch className="h-8 w-8 text-white" />
           </div>
-          <div className="flex justify-center flex-col pl-4">
+          <div className={styles.statCardContent}>
             <h2 className="font-medium">Notifications This Week</h2>
             <p className="text-2xl font-bold">107</p>
           </div>
