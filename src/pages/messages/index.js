756f5cfc91a941056c648f5d67b616428e97b5b5
John Carl James Nebab <ivan@orangeandcrane.com>
2021-03-05 13:53:21 +0800

    fix: fix 404 page when navigating to messages

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -2,7 +2,7 @@ import Messages from '@app/components/pages/messages'
 import Page from '@app/permissions/page'
 
 function MessagesPage() {
-  return <Page route="/message" page={<Messages />} />
+  return <Page route="/messages" page={<Messages />} />
 }
 
 export default MessagesPage
