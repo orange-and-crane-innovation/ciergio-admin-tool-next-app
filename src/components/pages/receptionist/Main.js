133157bd64dc3f5e336b180f56717ebcaafb4358
ednaaaj <jhondi.pamonag@gmail.com>
2021-03-22 19:14:21 +0800

    added settings tab and header

--- src/components/pages/receptionist/Main.js
+++ src/components/pages/receptionist/Main.js
@@ -2,10 +2,12 @@
 // import Deliveries from './deliveries'
 // import Services from './services'
 // import PickUps from './pick-ups'
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import Tabs from '@app/components/tabs'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import styles from './main.module.css'
+import { DateInput } from '@app/components/datetime'
 
 const _ = require('lodash')
 
@@ -26,30 +28,42 @@ const SETTINGS = {
 export default function Main() {
   const router = useRouter()
   const routerName = _.split(router.pathname, '/')[2]
+  const [selectedDate, setSelectedDate] = useState(new Date())
 
-  useEffect(() => {
-    const routerName = _.split(router.pathname, '/')[2]
-    console.log(SETTINGS[routerName])
-  }, [])
+  const handleDateChange = e => {
+    setSelectedDate(e)
+  }
 
   return (
     <>
-      <Tabs defaultTab="1">
-        <Tabs.TabLabels>
-          {SETTINGS[routerName] &&
-            SETTINGS[routerName].tabs.map((tab, index) => {
-              return (
-                <Tabs.TabLabel key={index} id={String(index + 1)}>
-                  <Link
-                    href={`/receptionist/${routerName}?${tab.toLowerCase()}`}
-                  >
-                    <a>{tab}</a>
-                  </Link>
-                </Tabs.TabLabel>
-              )
-            })}
-        </Tabs.TabLabels>
-      </Tabs>
+      <div className={styles.ReceptionistContainer}>
+        <h1 className={styles.ReceptionistHeading}>
+          Some building {routerName}
+        </h1>
+        <div className={styles.ReceotionistTableHeader}>
+          <DateInput
+            date={selectedDate}
+            onDateChange={handleDateChange}
+            dateFormat="MMMM DD, YYYY"
+          />
+        </div>
+        <Tabs defaultTab="1">
+          <Tabs.TabLabels>
+            {SETTINGS[routerName] &&
+              SETTINGS[routerName].tabs.map((tab, index) => {
+                return (
+                  <Tabs.TabLabel key={index} id={String(index + 1)}>
+                    <Link
+                      href={`/receptionist/${routerName}?${tab.toLowerCase()}`}
+                    >
+                      <a>{tab}</a>
+                    </Link>
+                  </Tabs.TabLabel>
+                )
+              })}
+          </Tabs.TabLabels>
+        </Tabs>
+      </div>
     </>
   )
 }
