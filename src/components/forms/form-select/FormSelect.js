6da5c009b80c60b6a14cd0bcd1d36bb2317eb4e5
John Carl James Nebab <nebab.johncarl@gmail.com>
2021-03-29 13:50:34 +0800

    fix: fix dropdown position

--- src/components/forms/form-select/FormSelect.js
+++ src/components/forms/form-select/FormSelect.js
@@ -77,6 +77,7 @@ const InputSelect = ({
               }
         }
         menuPlacement="auto"
+        menuPosition="fixed"
         closeMenuOnSelect={!isMulti}
         hideSelectedOptions={false}
         isMulti={isMulti}
