5c347a9606e41057b1a21e0ca0d59689807064fe
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-04-08 17:31:07 +0800

    fix: fix issue on loading input

--- src/components/uploader/index.js
+++ src/components/uploader/index.js
@@ -67,43 +67,53 @@ const Uploader = ({
     return parseFloat((size / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]
   }
 
-  if (fileUrls && fileUrls.length > 0) {
+  if (!error && fileUrls && fileUrls.length > 0) {
     uploadedFiles = fileUrls.map((file, index) => {
       return (
         <div key={index} className={containerClass}>
-          <div
-            className={styles.uploaderImage}
-            style={{
-              backgroundImage: `url(${!loading && file})`
-            }}
-          >
-            {files[index] && files[index].type === 'application/pdf' ? (
-              <ImagePdf />
-            ) : files[index] &&
-              files[index].type ===
-                'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ? (
-              <ImageDoc />
-            ) : null}
-          </div>
-          <button
-            type="button"
-            className={styles.uploaderButton}
-            data-id={file}
-            data-name={files[index] && files[index].name}
-            onClick={e => {
-              handleRemove()
-              onRemove(e)
-            }}
-          >
-            <FaRegTrashAlt />
-          </button>
-
-          {files[index] && files[index].name && (
+          {loading ? (
+            <div className={styles.uploaderImage}>
+              <FaSpinner className="icon-spin" />
+            </div>
+          ) : (
             <>
-              <div className={styles.uploaderContent}>{files[index].name}</div>
-              <div className="font-normal text-neutral-500">
-                ({getFileSize(files[index])})
+              <div
+                className={styles.uploaderImage}
+                style={{
+                  backgroundImage: `url(${!loading && file})`
+                }}
+              >
+                {files[index] && files[index].type === 'application/pdf' ? (
+                  <ImagePdf />
+                ) : files[index] &&
+                  files[index].type ===
+                    'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ? (
+                  <ImageDoc />
+                ) : null}
               </div>
+              <button
+                type="button"
+                className={styles.uploaderButton}
+                data-id={file}
+                data-name={files[index] && files[index].name}
+                onClick={e => {
+                  handleRemove()
+                  onRemove(e)
+                }}
+              >
+                <FaRegTrashAlt />
+              </button>
+
+              {files[index] && files[index].name && (
+                <>
+                  <div className={styles.uploaderContent}>
+                    {files[index].name}
+                  </div>
+                  <div className="font-normal text-neutral-500">
+                    ({getFileSize(files[index])})
+                  </div>
+                </>
+              )}
             </>
           )}
         </div>
@@ -115,7 +125,43 @@ const Uploader = ({
     <>
       <div className="flex">
         {uploadedFiles}
-        {fileUrls && fileUrls.length < maxFiles && (
+        {!error && maxFiles ? (
+          fileUrls &&
+          fileUrls.length < maxFiles && (
+            <div className={containerClass}>
+              <input
+                className={styles.uploaderControl}
+                type="file"
+                id="file"
+                name="file"
+                multiple={multiple}
+                onChange={onUpload}
+                accept={accept}
+              />
+              <div
+                className={styles.uploaderImage}
+                onClick={handleChange}
+                onDrop={handleOnDrop}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+              >
+                {loading ? (
+                  <FaSpinner className="icon-spin" />
+                ) : type === 'image' ? (
+                  <span className={styles.uploaderImageContent}>
+                    <ImageAdd />
+                    Add Image
+                  </span>
+                ) : (
+                  <span className={styles.uploaderImageContent}>
+                    <ImageFile />
+                    Add File
+                  </span>
+                )}
+              </div>
+            </div>
+          )
+        ) : (
           <div className={containerClass}>
             <input
               className={styles.uploaderControl}
@@ -158,7 +204,7 @@ const Uploader = ({
 Uploader.propTypes = {
   type: PropTypes.string,
   loading: PropTypes.bool,
-  error: PropTypes.bool,
+  error: PropTypes.string,
   files: PropTypes.array,
   fileUrls: PropTypes.array,
   multiple: PropTypes.bool,
