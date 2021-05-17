af793a85b63b25ebdb96932a215ad919aa6398a4
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-05-17 13:51:59 +0800

    chore: added clear and placeholder function for daterange

--- src/components/daterange/index.js
+++ src/components/daterange/index.js
@@ -2,7 +2,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react'
 import P from 'prop-types'
-import { DateRangePicker } from 'react-date-range'
+import { FaTimes } from 'react-icons/fa'
+import { DateRange, DateRangePicker } from 'react-date-range'
 
 import FormInput from '@app/components/forms/form-input'
 import Button from '@app/components/button'
@@ -14,12 +15,17 @@ import 'react-date-range/dist/theme/default.css'
 import styles from './index.module.css'
 
 const DateRangeInput = ({
+  placeholder,
   isShown,
   onDateChange,
   onDateApply,
-  hasApplyButton
+  onDateClear,
+  hasApplyButton,
+  hasSideOptions,
+  hasClear
 }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [isEmpty, setIsEmpty] = useState(true)
   const [selectedDateRange, setSelectedDateRange] = useState([
     {
       startDate: new Date(),
@@ -35,10 +41,16 @@ const DateRangeInput = ({
   const handleChange = e => {
     setSelectedDateRange([e.selection])
     onDateChange([e.selection])
+    setIsEmpty(false)
+  }
+
+  const handleClear = e => {
+    onDateClear()
+    setIsEmpty(true)
   }
 
   return (
-    <>
+    <div className={styles.DateRangeMainContainer}>
       <div
         className={`${styles.DateRangeContainer} ${
           isShown ? 'block' : 'hidden'
@@ -47,26 +59,52 @@ const DateRangeInput = ({
         <div className="cursor-pointer" onClick={handleCalendar}>
           <FormInput
             name="date"
+            inputProps={{
+              style: { backgroundColor: 'white' }
+            }}
             inputClassName="pointer-events-none"
-            value={`${DATE.toFriendlyShortDate(
-              selectedDateRange[0].startDate
-            )} - ${DATE.toFriendlyShortDate(selectedDateRange[0].endDate)}`}
+            placeholder={placeholder}
+            value={
+              !isEmpty
+                ? `${DATE.toFriendlyShortDate(
+                    selectedDateRange[0].startDate
+                  )} - ${DATE.toFriendlyShortDate(
+                    selectedDateRange[0].endDate
+                  )}`
+                : ''
+            }
             onChange={() => {}}
             readOnly
           />
           <i className={`ciergio-calendar ${styles.Icon}`} />
         </div>
 
+        {hasClear && !isEmpty && selectedDateRange[0]?.startDate && (
+          <FaTimes className={styles.CloseIcon} onClick={handleClear} />
+        )}
+
         <div className={`${styles.Content} ${isOpen && styles.Open}`}>
-          <DateRangePicker
-            editableDateInputs={true}
-            onChange={handleChange}
-            moveRangeOnFirstSelection={false}
-            ranges={selectedDateRange}
-            showSelectionPreview={true}
-            color="#F56222"
-            rangeColors={['#F56222']}
-          />
+          {hasSideOptions ? (
+            <DateRangePicker
+              editableDateInputs={true}
+              onChange={handleChange}
+              moveRangeOnFirstSelection={false}
+              ranges={selectedDateRange}
+              showSelectionPreview={true}
+              color="#F56222"
+              rangeColors={['#F56222']}
+            />
+          ) : (
+            <DateRange
+              editableDateInputs={true}
+              onChange={handleChange}
+              moveRangeOnFirstSelection={false}
+              ranges={selectedDateRange}
+              showSelectionPreview={true}
+              color="#F56222"
+              rangeColors={['#F56222']}
+            />
+          )}
 
           <div className={styles.Button}>
             <Button
@@ -79,19 +117,30 @@ const DateRangeInput = ({
         </div>
       </div>
       {hasApplyButton && (
-        <div className={`mr-4 ${isShown ? 'block' : 'hidden'}`}>
+        <div className={`${isShown ? 'block' : 'hidden'}`}>
           <Button label="Apply" type="button" onClick={onDateApply} />
         </div>
       )}
-    </>
+    </div>
   )
 }
 
+DateRangeInput.defaultProps = {
+  isShown: true,
+  hasApplyButton: false,
+  hasSideOptions: true,
+  hasClear: false
+}
+
 DateRangeInput.propTypes = {
+  placeholder: P.string,
   isShown: P.bool,
   onDateChange: P.func,
   onDateApply: P.func,
-  hasApplyButton: P.bool
+  onDateClear: P.func,
+  hasApplyButton: P.bool,
+  hasSideOptions: P.bool,
+  hasClear: P.bool
 }
 
 export default DateRangeInput
