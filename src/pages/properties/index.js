a0659042cafb847838bd9b0b96bec94dd836da5e
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-06-01 14:30:34 +0800

    chore: update account type and name options

--- src/pages/properties/index.js
+++ src/pages/properties/index.js
@@ -1,18 +1,26 @@
 import { useRouter } from 'next/router'
 
+import { ACCOUNT_TYPES } from '@app/constants'
+
 function PropertiesPage() {
   const router = useRouter()
   const user = JSON.parse(localStorage.getItem('profile'))
   const accountType = user?.accounts?.data[0]?.accountType
 
-  if (accountType === 'administrator') {
+  if (accountType === ACCOUNT_TYPES.SUP.value) {
     router.push(`/properties/company`)
-  } else if (accountType === 'company_admin') {
+  } else if (accountType === ACCOUNT_TYPES.COMPYAD.value) {
     const id = user?.accounts?.data[0]?.company?._id
     router.push(`/properties/company/${id}/overview`)
-  } else if (accountType === 'complex_admin') {
+  } else if (accountType === ACCOUNT_TYPES.COMPXAD.value) {
     const id = user?.accounts?.data[0]?.complex?._id
     router.push(`/properties/complex/${id}/overview`)
+  } else if (
+    accountType === ACCOUNT_TYPES.BUIGAD.value ||
+    accountType === ACCOUNT_TYPES.RECEP.value
+  ) {
+    const id = user?.accounts?.data[0]?.building?._id
+    router.push(`/properties/building/${id}/overview`)
   }
 
   return null
