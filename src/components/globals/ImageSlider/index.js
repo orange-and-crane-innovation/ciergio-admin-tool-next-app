442ea6b86f556b21c1106cff5ff283ab25e4f516
Kelvin Ray Coloma <kelvin@orangeandcrane.com>
2021-07-29 13:16:05 +0800

    chore: added lightbox on imageslider

--- src/components/globals/ImageSlider/index.js
+++ src/components/globals/ImageSlider/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import ImageGallery from 'react-image-gallery'
 import 'react-image-gallery/styles/css/image-gallery.css'
 
+import LightBox from '@app/components/lightbox'
+
 import styles from './index.module.css'
 
-const SearchComponent = ({ images }) => {
+const ImageSlider = ({ images, onClick }) => {
+  const refImg = useRef(null)
+  const [imageLists, setImageLists] = useState([])
+  const [imageIndex, setImageIndex] = useState(0)
+  const [imageOpen, setImageOpen] = useState(false)
+
+  useEffect(() => {
+    if (images) {
+      setImageLists(images?.map(image => image.original))
+    }
+  }, [images])
+
+  const handleImageOpen = index => {
+    setImageOpen(true)
+    handleImageIndex(index)
+  }
+
+  const handleImageClose = () => {
+    setImageOpen(false)
+  }
+
+  const handleImageIndex = index => {
+    setImageIndex(index)
+  }
+
   const renderLeftNav = (onClick, disabled) => {
     return (
       <button
@@ -33,6 +59,7 @@ const SearchComponent = ({ images }) => {
   return (
     <div className={styles.ImageSliderContainer}>
       <ImageGallery
+        ref={refImg}
         items={images}
         showThumbnails={false}
         showPlayButton={false}
@@ -41,13 +68,22 @@ const SearchComponent = ({ images }) => {
         autoPlay={true}
         renderLeftNav={renderLeftNav}
         renderRightNav={renderRightNav}
+        onClick={() => handleImageOpen(refImg.current.getCurrentIndex())}
+      />
+      <LightBox
+        isOpen={imageOpen}
+        images={imageLists}
+        imageIndex={imageIndex}
+        onClick={handleImageIndex}
+        onClose={handleImageClose}
       />
     </div>
   )
 }
 
-SearchComponent.propTypes = {
-  images: PropTypes.array.isRequired
+ImageSlider.propTypes = {
+  images: PropTypes.array.isRequired,
+  onClick: PropTypes.func
 }
 
-export default SearchComponent
+export default ImageSlider
